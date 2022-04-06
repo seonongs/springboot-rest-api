0957ba3e2a9326496881a9be19fef64824cf2464
seonongs <seonongs@gmail.com>
2022-04-06 13:54:55 +0900

    .

--- src/main/java/com/springbootrestapi/board/BoardController.java
+++ src/main/java/com/springbootrestapi/board/BoardController.java
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@CrossOrigin(origins = "http://localhost:3333")
+@CrossOrigin(origins = "http://localhost")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
